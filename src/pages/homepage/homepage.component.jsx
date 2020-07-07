ce8ef323d0e283b34a753cd76e6df64eb8aa6637
Liyi <xmage211120@gmail.com>
2020-07-07 13:02:53 +0800

    add more flexbox features

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -12,8 +12,40 @@ function Homepage() {
     padding: 0,
   });
 
+  const [flexBox, setFlexBox] = useState({
+    flexDirection: "row",
+    alignItems: "flex-start",
+    justifyContent: "start",
+    flex: "auto",
+  });
+
+  // const [componentProps, setComponentProps] = useState({
+  //   fixSize: {
+  //     width: "200px",
+  //     height: "200px",
+  //   },
+  //   flexSize: {
+  //     minWidth: "200px",
+  //     minHeight: "200px",
+  //   },
+  // });
+
   const handleBoxModelChange = (event) => {
     setBoxModel({ ...boxModel, [event.target.name]: event.target.value });
+  };
+
+  const containerStyle = {
+    display: `${display.parentDisplay}`,
+    flexDirection: flexBox.flexDirection,
+    alignItems: flexBox.alignItems,
+    justifyContent: flexBox.justifyContent,
+  };
+
+  const componentStyle = {
+    margin: `${boxModel.margin}px`,
+    padding: `${boxModel.padding}px`,
+    display: display.childDisplay,
+    // flex: flexBox.flex,
   };
 
   return (
@@ -44,6 +76,178 @@ function Homepage() {
             }
           />
           <label>flex</label>
+        </div>
+
+        <div className="flexBoxController">
+          <h3>Flexbox</h3>
+          {/* --------------------flex-direction-------------------- */}
+
+          <div className="radioContainer">
+            <input
+              type="radio"
+              name="flexbox-flex-direction"
+              onChange={() =>
+                setFlexBox({
+                  ...flexBox,
+                  flexDirection: "row",
+                })
+              }
+            />
+            <label>flex-direction:row</label>
+          </div>
+
+          <div className="radioContainer">
+            <input
+              type="radio"
+              name="flexbox-flex-direction"
+              onChange={() =>
+                setFlexBox({
+                  ...flexBox,
+                  flexDirection: "column",
+                })
+              }
+            />
+            <label>flex-direction:column</label>
+          </div>
+          <br />
+          {/* --------------------align-items-------------------- */}
+          <div className="radioContainer">
+            <input
+              type="radio"
+              name="flexbox-align-items"
+              onChange={() =>
+                setFlexBox({
+                  ...flexBox,
+                  alignItems: "flex-start",
+                })
+              }
+            />
+            <label>align-items:flex-start</label>
+          </div>
+
+          <div className="radioContainer">
+            <input
+              type="radio"
+              name="flexbox-align-items"
+              onChange={() =>
+                setFlexBox({
+                  ...flexBox,
+                  alignItems: "center",
+                })
+              }
+            />
+            <label>align-items:center</label>
+          </div>
+
+          <div className="radioContainer">
+            <input
+              type="radio"
+              name="flexbox-align-items"
+              onChange={() =>
+                setFlexBox({
+                  ...flexBox,
+                  alignItems: "flex-end",
+                })
+              }
+            />
+            <label>align-items:flex-end</label>
+          </div>
+
+          <div className="radioContainer">
+            <input
+              type="radio"
+              name="flexbox-align-items"
+              onChange={() =>
+                setFlexBox({
+                  ...flexBox,
+                  alignItems: "stretch",
+                })
+              }
+            />
+            <label>align-items:stretch</label>
+          </div>
+          <br />
+          {/* --------------------justify-content-------------------- */}
+          <div className="radioContainer">
+            <input
+              type="radio"
+              name="flexbox-justify-content"
+              onChange={() =>
+                setFlexBox({
+                  ...flexBox,
+                  justifyContent: "flex-start",
+                })
+              }
+            />
+            <label>justity-content:flex-start</label>
+          </div>
+
+          <div className="radioContainer">
+            <input
+              type="radio"
+              name="flexbox-justify-content"
+              onChange={() =>
+                setFlexBox({
+                  ...flexBox,
+                  justifyContent: "center",
+                })
+              }
+            />
+            <label>justity-content:center</label>
+          </div>
+
+          <div className="radioContainer">
+            <input
+              type="radio"
+              name="flexbox-justify-content"
+              onChange={() =>
+                setFlexBox({
+                  ...flexBox,
+                  justifyContent: "flex-end",
+                })
+              }
+            />
+            <label>justity-content:flex-end</label>
+          </div>
+          <div className="radioContainer">
+            <input
+              type="radio"
+              name="flexbox-justify-content"
+              onChange={() =>
+                setFlexBox({
+                  ...flexBox,
+                  justifyContent: "space-between",
+                })
+              }
+            />
+            <label>justity-content:space-between</label>
+          </div>
+          <div className="radioContainer">
+            <input
+              type="radio"
+              name="flexbox-justify-content"
+              onChange={() =>
+                setFlexBox({
+                  ...flexBox,
+                  justifyContent: "space-around",
+                })
+              }
+            />
+            <label>justity-content:space-around</label>
+          </div>
+          <div className="radioContainer">
+            <input
+              type="radio"
+              name="flexbox-justify-content"
+              onChange={() =>
+                setFlexBox({
+                  ...flexBox,
+                  justifyContent: "space-evenly",
+                })
+              }
+            />
+            <label>justity-content:space-evenly</label>
+          </div>
         </div>
         <div className="childController">
           <h3>Child</h3>
@@ -95,38 +299,14 @@ function Homepage() {
         </div>
       </div>
       {/* <Component /> */}
-      <div
-        className="elementContainer"
-        style={{ display: `${display.parentDisplay}` }}
-      >
-        <div
-          className="component A"
-          style={{
-            margin: `${boxModel.margin}px`,
-            padding: `${boxModel.padding}px`,
-            display: `${display.childDisplay}`,
-          }}
-        >
+      <div className="elementContainer" style={containerStyle}>
+        <div className="component A" style={componentStyle}>
           A
         </div>
-        <div
-          className="component B"
-          style={{
-            margin: `${boxModel.margin}px`,
-            padding: `${boxModel.padding}px`,
-            display: `${display.childDisplay}`,
-          }}
-        >
+        <div className="component B" style={componentStyle}>
           B
         </div>
-        <div
-          className="component C"
-          style={{
-            margin: `${boxModel.margin}px`,
-            padding: `${boxModel.padding}px`,
-            display: `${display.childDisplay}`,
-          }}
-        >
+        <div className="component C" style={componentStyle}>
           C
         </div>
       </div>
